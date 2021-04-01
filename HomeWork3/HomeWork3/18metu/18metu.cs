b16b4fbea073a990bc5bf7c81aabbc5612caa9ba
Alfonsas-Rusilas <alfonsas.rusilas@gmail.com>
2021-04-01 18:20:24 +0300

    pirmas

---
+++ HomeWork3/HomeWork3/18metu/18metu.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork3._18metu
+{
+    class _18metu
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Įveskite metus: ");
+            int amzius = Convert.ToInt32(Console.ReadLine());
+
+            if (amzius >=18)
+            {
+                Console.WriteLine("Jūs turite bent 18 metų");
+            }
+            else
+            {
+                Console.WriteLine("Jūs per jaunas");
+            }
+
+            if (amzius % 2 ==0)
+            {
+                Console.WriteLine("Jūsų metų skaičius lyginis");
+            }
+            else
+            {
+                Console.WriteLine("Jūsų metų skaičius nelyginis");
+            }
+
+        }
+    }
+}
---
+++ HomeWork3/HomeWork3/MokinioPazymys/MokinioPazymys.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork3.MokinioPazymys
+{
+    class MokinioPazymys
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Įveskite pažymį: ");
+            int paz = Convert.ToInt32(Console.ReadLine());
+
+            if (paz>10 || paz <1)
+            {
+                Console.WriteLine("Klaida.");
+            }
+            else if (paz == 10)
+            {
+                Console.WriteLine("puiku");
+            }
+            else if (paz == 9 || paz ==8)
+            {
+                Console.WriteLine("labai gerai");
+            }
+            else if (paz == 7 || paz == 6)
+            {
+                Console.WriteLine("gerai");
+            }
+            else if (paz == 5)
+            {
+                Console.WriteLine("vidutiniškai");
+            }
+            else if (paz == 4)
+            {
+                Console.WriteLine("bent teigiamai");
+            }
+            else 
+            {
+                Console.WriteLine("Labai blogai");
+            }
+            Console.Write("Programos pabaiga.");
+        }
+    }
+}
